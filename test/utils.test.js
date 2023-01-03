80f884e904224981ca583443aff270614d90ce2e
Candace Ahrends <candaceahrends@Candaces-MacBook-Pro-2.local>
2023-01-03 17:16:50 -0500

    refactoring

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,35 +1,34 @@
-import {isTimeFormat,zeroPadTime, formatTime, formatTimeFromHourMinute} from "../lib/time-picker/utils"
+import {
+  isTimeFormat,
+  zeroPadTime,
+  formatUserInputTime,
+  formatTimeFromHourMinute,
+} from "../lib/time-picker/utils";
 
+describe("Utils Tests", () => {
+  test("should detect invalid time formats", () => {
+    let isOk = isTimeFormat("not a time format");
+    expect(isOk).toBeFalsy();
 
-describe( "Utils Tests", ()=>{
+    isOk = isTimeFormat("66:77");
+    expect(isOk).toBeFalsy();
+  });
 
-    test ("should detect invalid time formats", ()=>{
-        let isOk = isTimeFormat("not a time format")
-        expect(isOk).toBeFalsy()
+  test("should detect valid formats", () => {
+    let isOk = isTimeFormat("3:30");
+    expect(isOk).toBeFalsy();
 
-        isOk = isTimeFormat("66:77")
-        expect(isOk).toBeFalsy()
+    isOk = isTimeFormat("6:08");
+    expect(isOk).toBeFalsy();
+  });
 
+  test("should zeroPadTime a time", () => {
+    let time = zeroPadTime("9:0");
+    expect(time).toEqual("09:00");
+  });
 
-    })
-
-    test ("should detect valid formats", ()=>{
-        let isOk = isTimeFormat("3:30")
-        expect(isOk).toBeFalsy()
-
-        isOk = isTimeFormat("6:08")
-        expect(isOk).toBeFalsy()
-
-
-    })
-
-    test ("should zeroPadTime a time", ()=>{
-        let time = zeroPadTime("9:0")
-        expect(time).toEqual("09:00")
-
-        
-
-    })
-
-
-})
+  test("should format time", () => {
+    const time = formatUserInputTime("3:30");
+    expect(time).toEqual("03:30");
+  });
+});
