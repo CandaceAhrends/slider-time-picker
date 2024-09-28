ec3d5122ce8beeb994d6fa9d74e9ce94d5d5da80
Candace Ahrends <candaceahrends@Candaces-MacBook-Pro.local>
2024-09-28 17:20:21 -0400

    touch up code

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+//import TimePicker from "../lib/time-picker/components/TimePicker";
+import TimePicker from "slider-time-picker";
+import "slider-time-picker/dist/sliderTimePicker.css";
+
+function App() {
+  const [testOutput, setTestOutput] = React.useState("initial");
+  const onChange = (time) => {
+    console.log(time);
+    setTestOutput(time);
+  };
+  return (
+    <div className="App">
+      {/* <TimePicker onChange={onChange}></TimePicker> */}
+
+      <div
+        style={{
+          margin: "3rem",
+          display: "flex",
+          alignItems: "center",
+          padding: "5rem",
+        }}
+      >
+        <TimePicker onChange={onChange} disabled={false}></TimePicker>
+        <div style={{ margin: "0 5rem", fontSize: "1.5rem" }}>
+          <p> {testOutput}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
