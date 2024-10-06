07aca3a183fdbf9d065d58e635623eb8e25daca8
Candace Ahrends <candaceahrends@Candaces-MacBook-Pro.local>
2024-10-06 09:56:42 -0400

    all all colors to be passed in to the component in a color object

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import "./app.scss";
 import TimePicker from "slider-time-picker";
 import "slider-time-picker/dist/sliderTimePicker.css";
 
+const colors = {
+  "primary-color": "black",
+  "primary-font-color": "pink",
+  "background-color": "#1e2528",
+  "slider-track-color": "red",
+  "time-input-background-color": "green",
+};
 function App() {
   const [testOutput, setTestOutput] = React.useState("initial");
   const onChange = (time) => {
@@ -20,7 +27,11 @@ function App() {
           padding: "5rem",
         }}
       >
-        <TimePicker onChange={onChange} disabled={false}></TimePicker>
+        <TimePicker
+          onChange={onChange}
+          disabled={false}
+          colors={colors}
+        ></TimePicker>
         <div style={{ margin: "0 5rem", fontSize: "1.5rem" }}>
           <p> {testOutput}</p>
         </div>
